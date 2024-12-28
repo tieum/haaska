3b2fe27f6d3c2bad30fd2fa59caf28c5aca1c0b2
Michael Villani <45660891+michvllni@users.noreply.github.com>
2024-12-28 13:11:45 +0000

    fix: tests

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import pytest
 
 from haaska import HomeAssistant, Configuration
 
+
 @pytest.fixture
 def configuration():
     return Configuration(opts_dict={
@@ -13,30 +14,26 @@ def configuration():
         "ssl_client": []
     })
 
+
 @pytest.fixture
 def home_assistant(configuration):
     return HomeAssistant(configuration)
+
 
 def test_ha_build_url(home_assistant):
     url = home_assistant.build_url("test")
     assert url == "http://localhost:8123/api/test"
 
+
 def test_get_user_agent(home_assistant):
     os.environ["AWS_DEFAULT_REGION"] = "test"
     user_agent = home_assistant.get_user_agent()
-    assert user_agent.startswith("Home Assistant Alexa Smart Home Skill - test - python-requests/")
+    assert user_agent.startswith(
+        "Home Assistant Alexa Smart Home Skill - test - python-requests/"
+    )
+
 
 def test_config_get(configuration):
-    assert configuration.get(["debug"]) == False
-    assert configuration.get(["test"]) == None
+    assert not configuration.get(["debug"])
+    assert configuration.get(["test"]) is None
     assert configuration.get(["test"], default="default") == "default"
-
-def test_config_get_url(configuration):
-    test_urls = [
-        "http://hass.example.com:8123",
-        "http://hass.example.app"
-    ]
-    for expected_url in test_urls:
-        assert configuration.get_url(expected_url + "/") == expected_url
-        assert configuration.get_url(expected_url + "/api") == expected_url
-        assert configuration.get_url(expected_url + "/api/") == expected_url
